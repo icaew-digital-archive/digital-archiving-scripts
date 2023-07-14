f3645d5cc7879d6aa615c044bed74e66abae16ee
Craig McCarthy <craig.mccarthy@icaew.com>
2023-07-14 16:17:32 +0100

    Refactored and now includes CLI arguments

--- browsertrix-crawler files and scripts/crawl-sanity-check.py
+++ browsertrix-crawler files and scripts/crawl-sanity-check.py
@@ -5,31 +5,59 @@
 Checks against list of URLs given to browsertrix with URLs crawled reported by the pages.jsonl log.
 """
 
+import argparse
 import json
 
-URL_LIST_FILE = ''
-PAGES_JSON = ''
 
-# Read URL file into list
-with open(URL_LIST_FILE, 'r') as f:
-    url_list = [line.strip() for line in f]
+def read_file_into_list(file_path):
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file]
 
-# Read JSON from pages.jsonl file
-with open(PAGES_JSON, 'r') as f:
-    next(f)  # Skip first line
-    pages_jsonl_data = [json.loads(line) for line in f]
 
-# Create empty list to store the URLs from pages.jsonl
-pages_jsonl_data_url_list = []
+def load_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        next(file)  # Skip first line
+        return [json.loads(line) for line in file]
 
-# Loop through pages_jsonl_data to append
-for entry in pages_jsonl_data:
-    # Print to console page titles and URLs
-    if entry['url'] in url_list:
-        print(entry['title'], '\n', entry['url'], '\n')
-    pages_jsonl_data_url_list.append(entry['url'])
 
-# Print missing URLs if any
-for url in url_list:
-    if url not in pages_jsonl_data_url_list:
-        print('\nMISSING:', url)
+def print_matching_urls(url_list, pages_data):
+    pages_data_url_list = []
+    for entry in pages_data:
+        if entry['url'] in url_list:
+            print(entry['title'], '\n', entry['url'], '\n')
+        pages_data_url_list.append(entry['url'])
+    return pages_data_url_list
+
+
+def print_missing_urls(url_list, pages_data_url_list):
+    missing_urls = [url for url in url_list if url not in pages_data_url_list]
+    print('URLs not found in pages.jsonl but present in the URL list file:')
+    for url in missing_urls:
+        print(url)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Check URLs against crawled data')
+    parser.add_argument('url_list', help='Path to the URL list file')
+    parser.add_argument('pages_jsonl', help='Path to the pages.jsonl file')
+    args = parser.parse_args()
+
+    url_list_file = args.url_list
+    pages_json_file = args.pages_jsonl
+
+    # Read URL file into list
+    url_list = read_file_into_list(url_list_file)
+
+    # Read JSON from pages.jsonl file
+    pages_jsonl_data = load_jsonl(pages_json_file)
+
+    # Print matching URLs
+    pages_jsonl_data_url_list = print_matching_urls(url_list, pages_jsonl_data)
+
+    # Print missing URLs if any
+    print_missing_urls(url_list, pages_jsonl_data_url_list)
+
+
+if __name__ == '__main__':
+    main()
