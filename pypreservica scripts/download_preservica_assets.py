1358d66a30a0cdb95d8d6ed25a9b9c2065b8bfb5
Craig McCarthy <craig.mccarthy@icaew.com>
2023-11-28 11:10:36 +0000

    Added SHA1 fixity checking and refactored

--- pypreservica scripts/download_preservica_assets.py
+++ pypreservica scripts/download_preservica_assets.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Download Preservica assets from a given parent folder.
+Download Preservica assets from a given parent folder. Checks SHA1 fixity values from Preservica and local file match.
 
 Enter "root" as argument to preservica_folder_ref if needing to download from the root level.
 
@@ -12,6 +12,8 @@ usage: download_preservica_assets.py [-h] preservica_folder_ref download_folder
 import argparse
 import os
 from pathlib import Path
+import hashlib
+import logging
 
 from dotenv import load_dotenv
 from pyPreservica import *
@@ -22,6 +24,40 @@ USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 TENANT = os.getenv('TENANT')
 SERVER = os.getenv('SERVER')
+
+# Configure logging
+# You can set the level according to your needs
+logging.basicConfig(level=logging.INFO)
+
+
+def calculate_file_sha1(file_path):
+    sha1 = hashlib.sha1()
+    with open(file_path, 'rb') as file:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: file.read(4096), b""):
+            sha1.update(byte_block)
+    return sha1.hexdigest()
+
+
+def download_bitstream(client, bitstream, download_path):
+    try:
+        client.bitstream_content(bitstream, download_path)
+        return True
+    except Exception as e:
+        logging.error(f"Error downloading {bitstream.filename}: {e}")
+        return False
+
+
+def check_fixity(downloaded_path, preservica_sha1):
+    if preservica_sha1:
+        downloaded_sha1 = calculate_file_sha1(downloaded_path)
+        if downloaded_sha1 == preservica_sha1:
+            logging.info(f"Fixity values match ({preservica_sha1})")
+            return True
+        else:
+            logging.error('Fixity values did not match.')
+            return False
+    return True
 
 
 def main(args):
@@ -36,19 +72,24 @@ def main(args):
         args.preservica_folder_ref = None
 
     for asset in filter(only_assets, client.all_descendants(args.preservica_folder_ref)):
-        print(f'Downloading {asset.title} ({asset.reference})')
+        logging.info(f'Downloading {asset.title} ({asset.reference})')
 
         for representation in client.representations(asset):
             for content_object in client.content_objects(representation):
                 for generation in client.generations(content_object):
                     for bitstream in generation.bitstreams:
-                        download_path = Path(
-                            args.download_folder) / bitstream.filename
-                        try:
-                            client.bitstream_content(bitstream, download_path)
-                        except Exception as e:
-                            print(
-                                f"Error downloading {bitstream.filename}: {e}")
+                        for algorithm, value in bitstream.fixity.items():
+                            if algorithm == 'SHA1':
+                                preservica_sha1 = value
+                            else:
+                                preservica_sha1 = False
+
+                        download_path = os.path.join(
+                            args.download_folder, bitstream.filename)
+
+                        if download_bitstream(client, bitstream, download_path):
+                            if not check_fixity(download_path, preservica_sha1):
+                                sys.exit()
 
 
 if __name__ == "__main__":
