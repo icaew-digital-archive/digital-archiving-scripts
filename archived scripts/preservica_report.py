c9aa8e1bf3a021ff3a502d0327fb52646dfe1aa1
Craig McCarthy <craig.mccarthy@icaew.com>
2024-02-09 11:45:00 +0000

    -

--- archived scripts/preservica_report.py
+++ archived scripts/preservica_report.py
@@ -9,6 +9,11 @@ The CSV file contains the following fields: Folder/Asset path, entity.title, ent
 import csv
 import os
 import xml.etree.ElementTree as ET
+import logging
+
+# Configure logging to write to a file with a basic format, set the log level to ERROR
+logging.basicConfig(filename='error_log.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 from dotenv import load_dotenv
 from pyPreservica import *
@@ -24,9 +29,9 @@ SERVER = os.getenv('SERVER')
 client = EntityAPI(username=USERNAME,
                    password=PASSWORD, tenant=TENANT, server=SERVER)
 
-CSV_OUTPUT_FILENAME = 'testing3.csv'
+CSV_OUTPUT_FILENAME = '20240206-report.csv'
 # PARENT_FOLDER_REF: None for root, or a specific folder reference number such as - 'a9c9fa31-4842-4ff3-9dad-d0ae2fbe6c28'
-PARENT_FOLDER_REF = 'e5407514-3209-4668-874b-4eac4a91b0a8'
+PARENT_FOLDER_REF = None
 
 
 def main():
@@ -40,55 +45,61 @@ def main():
         # Filter can be applied via: for asset in filter(only_assets, client.all_descendants()):
         for entity in client.all_descendants(PARENT_FOLDER_REF):
 
-            # Create directory path to root
-            if entity.parent != None:  # Gets around calling entity.parent etc. on root implied root reference in all_descendants()
-                folderpath = []
-                folder = client.folder(entity.parent)
-                folderpath.insert(0, folder.title + '/')
-                while folder.parent != None:
-                    folder = client.folder(folder.parent)
+            try:
+
+                # Create directory path to root
+                if entity.parent != None:  # Gets around calling entity.parent etc. on root implied root reference in all_descendants()
+                    folderpath = []
+                    folder = client.folder(entity.parent)
                     folderpath.insert(0, folder.title + '/')
-                folder_and_assetpath = ''.join(folderpath) + entity.title
-            else:
-                folder_and_assetpath = entity.title
-            print(folder_and_assetpath + '\n')
+                    while folder.parent != None:
+                        folder = client.folder(folder.parent)
+                        folderpath.insert(0, folder.title + '/')
+                    folder_and_assetpath = ''.join(folderpath) + entity.title
+                else:
+                    folder_and_assetpath = entity.title
+                print(folder_and_assetpath + '\n')
 
-            # Folder logic
-            if str(entity.entity_type) == 'EntityType.FOLDER':
-                asset = client.folder(entity.reference)
-                security_tag = asset.security_tag
-                for metadata in client.all_metadata(asset):
-                    schema = metadata[0]
-                    if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
-                        xml_string = metadata[1]
-                        root = ET.fromstring(xml_string)
-                        title_metadata = root[0].text
-                    else:
-                        title_metadata = ''
+                # Folder logic
+                if str(entity.entity_type) == 'EntityType.FOLDER':
+                    asset = client.folder(entity.reference)
+                    security_tag = asset.security_tag
+                    for metadata in client.all_metadata(asset):
+                        schema = metadata[0]
+                        if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
+                            xml_string = metadata[1]
+                            root = ET.fromstring(xml_string)
+                            title_metadata = root[0].text
+                        else:
+                            title_metadata = ''
 
-            # Asset logic
-            if str(entity.entity_type) == 'EntityType.ASSET':
-                asset = client.asset(entity.reference)
-                security_tag = asset.security_tag
-                for metadata in client.all_metadata(asset):
-                    schema = metadata[0]
-                    if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
-                        xml_string = metadata[1]
-                        root = ET.fromstring(xml_string)
-                        title_metadata = root[0].text
-                        print(root[0].text)
-                        print(root[1].text)
-                        print(root[2].text)
-                        print(root[3].text)
-                        print(root[4].text)
-                        print(root[5].text)
-                        print(root[6].text)
-                        print(root[7].text)
-                        print(root[8].text)
-                    else:
-                        title_metadata = ''
+                # Asset logic
+                if str(entity.entity_type) == 'EntityType.ASSET':
+                    asset = client.asset(entity.reference)
+                    security_tag = asset.security_tag
+                    for metadata in client.all_metadata(asset):
+                        schema = metadata[0]
+                        if schema == 'http://www.openarchives.org/OAI/2.0/oai_dc/':
+                            xml_string = metadata[1]
+                            root = ET.fromstring(xml_string)
+                            title_metadata = root[0].text
+                            print(root[0].text)
+                            print(root[1].text)
+                            print(root[2].text)
+                            print(root[3].text)
+                            print(root[4].text)
+                            print(root[5].text)
+                            print(root[6].text)
+                            print(root[7].text)
+                            print(root[8].text)
+                        else:
+                            title_metadata = ''
 
-            writer.writerow([folder_and_assetpath, entity.title, entity.reference, entity.entity_type, security_tag, title_metadata])
+                writer.writerow([folder_and_assetpath, entity.title, entity.reference, entity.entity_type, security_tag, title_metadata])
+            
+            except Exception as e:
+                # Log the exception
+                logging.error("An error occurred: %s", e)
 
 
 if __name__ == "__main__":
